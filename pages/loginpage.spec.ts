a1e0a174ab4f78b223fc06ba1f73954e27021d7b
sabbir <sbbrcse@gmail.com>
2026-05-20 16:05:56 +0600

    POM initiated

---
+++ pages/loginpage.spec.ts
@@ -0,0 +1,76 @@
+import {Page, expect} from '@playwright/test';
+import{urls, _pageTitle} from "../testData/helper.data.";
+
+export class LoginPage {
+
+    private page : Page;
+
+    //Locators
+
+    private _emailfield = 'input[name="username"]';
+    private _passwordfield = 'input[name="password"]';
+    private _submitbutton = "button[type='submit']";
+
+    constructor(page:Page) {
+        this.page = page;
+    }
+
+    //Action
+
+    async navigationTo()
+    {
+        await this.page.goto(urls._baseUrl + urls._loginPage);
+        await this.page.waitForLoadState('domcontentloaded');
+    }
+
+    //Fill Mail
+
+    async fillmail(email: string)
+    {
+        await this.page.locator(this._emailfield).fill(email);
+    }
+
+    //Fill Password
+
+    async fillpassword (password: string)
+    {
+        await this.page.locator(this._passwordfield).fill(password);
+    }
+
+    // Click Submit
+
+    async clicksubmit()
+    {
+        await this.page.locator(this._submitbutton).click();
+    }
+
+    //Full Login Action
+
+    async login(email : string, password: string)
+    {
+        await this.fillmail(email);
+        await this.fillpassword(password);
+        await this.clicksubmit();
+
+        // 1 min wait
+        await this.page.waitForLoadState('networkidle', {timeout: 60000});
+    }
+
+    //Assertion
+
+   async verifyPageLoaded()
+   {
+       await expect(this.page).toHaveTitle(_pageTitle);
+       await expect(this.page).toHaveURL(urls._baseUrl + urls._loginPage);
+   }
+
+   async verifyLoginSucess()
+   {
+       await expect(this.page).not.toHaveURL(/login/);
+   }
+   async verifyLoginFailed()
+   {
+       await expect(this.page).toHaveURL(/login/);
+   }
+
+}
---
+++ testData/helper.data..ts
@@ -0,0 +1,17 @@
+export const test_credentials =
+    {
+        _email: 'sabbir.ahmed@solshare.com',
+        _password: 'S@bbir4hmed',
+        _wrongEmail: 'sabbir.ahmed@gmail.com',
+        _wrongPassword: 'S@bbirhmed',
+    };
+
+export const urls =
+    {
+       _baseUrl : 'https://www.emobility-stage.me-solshare.net/auth/login',
+        _loginPage: '/auth/login',
+        _dashboardPage: '/home',
+
+    };
+
+export const _pageTitle = 'ME SOLshare';
